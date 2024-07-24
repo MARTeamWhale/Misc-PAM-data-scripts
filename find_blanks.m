6b028d1892f84fda0ebebb63d9d947f8b9be3245
Mike-J-Adams <78365898+Mike-J-Adams@users.noreply.github.com>
2024-07-24 08:55:32 -0300

    Update find_blanks.m

--- find_blanks.m
+++ find_blanks.m
@@ -6,7 +6,7 @@
 %CHANGE AS NEEDED
 
 %Enter data folder
-PATH2DATA = "\\142.2.83.52\whalenas1\MOORED_PAM_DATA\2018\09\LOC_2018_09\AMAR376.1.32000";
+%PATH2DATA = "\\142.2.83.52\whalenas1\MOORED_PAM_DATA\2018\09\LOC_2018_09\AMAR376.1.32000";
 
 filelist = dir(fullfile(PATH2DATA,'**/*.wav'));
 filetable = table(filelist);
@@ -18,7 +18,7 @@ for f = 1:length(filelist)
     tic
     [x,Fs] = audioread(fullfile(filelist(f).folder,filelist(f).name),[1,10]);
     if max(x) == 0
-        filetable(f).blank = 1;
+        filetable.blank(f) = 1;
     end
     toc
 end
